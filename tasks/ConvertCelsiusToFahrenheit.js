07e8fcf167055216e9d7954a82ccf444fd649902
Aleksandr Khischenko <khischenko.a@gmail.com>
2020-12-04 10:52:04 +0200

    Basic Algorithm Scripting: Convert Celsius to Fahrenheit

---
+++ tasks/ConvertCelsiusToFahrenheit.js
@@ -0,0 +1,17 @@
+/*
+Task
+Basic Algorithm Scripting: Convert Celsius to Fahrenheit
+*/
+
+// Variable declaration
+var result;
+
+// Function declaration
+function convertToFahrenheit(celsius) {
+  let fahrenheit;
+  fahrenheit = celsius * 9 / 5 + 32;
+  return fahrenheit;
+}
+
+result = convertToFahrenheit(30);
+console.log(`Your result: ${result}F`);
