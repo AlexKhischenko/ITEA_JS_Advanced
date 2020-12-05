b1f9f9f3808a1e56d397a2c342e52ebbb7a8d882
Aleksandr Khischenko <khischenko.a@gmail.com>
2020-12-05 22:02:13 +0200

    ConvertCelsiusToFahrenheit - code refactoring

--- tasks/ConvertCelsiusToFahrenheit.js
+++ tasks/ConvertCelsiusToFahrenheit.js
@@ -3,15 +3,9 @@ Task
 Algorithm Scripting: Convert Celsius to Fahrenheit
 */
 
-// Variable declaration
-var result;
-
 // Function declaration
 function convertToFahrenheit(celsius) {
-  let fahrenheit;
-  fahrenheit = celsius * 9 / 5 + 32;
-  return fahrenheit;
+  return celsius * 9 / 5 + 32;
 }
 
-result = convertToFahrenheit(30);
-console.log(`Your result: ${result}F`);
+console.log(`Your result: ${convertToFahrenheit(30)}F`);
