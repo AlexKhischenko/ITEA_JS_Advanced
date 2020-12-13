9bb60dc00a1655fe0152d8f384c4d108e1454bbd
Aleksandr Khischenko <khischenko.a@gmail.com>
2020-12-13 12:05:34 +0200

    added file BooWho.js

---
+++ tasks/BooWho.js
@@ -0,0 +1,48 @@
+/*
+Task
+Algorithm Scripting: Boo who
+Check if a value is classified as a boolean primitive. Return true or false.
+*/
+
+/* ---------- Imperative method ---------- */
+
+function booWho(bool) {
+  switch (bool) {
+    case true:
+    case false:
+      return true;
+    default:
+      return false;
+  }
+}
+
+console.log(booWho(null)); /* should return false */
+console.log(booWho(true)); /* should return true */
+console.log(booWho(false)); /* should return true */
+console.log(booWho([1, 2, 3])); /* should return false */
+console.log(booWho([].slice)); /* should return false */
+console.log(booWho({ "a": 1 })); /* should return false */
+console.log(booWho(1)); /* should return false */
+console.log(booWho(NaN)); /* should return false */
+console.log(booWho("a")); /* should return false */
+console.log(booWho("true")); /* should return false */
+console.log(booWho("false")); /* should return false */
+
+
+/* ---------- Declarative method ---------- */
+
+// function booWho(bool) {
+//   return typeof(bool) === 'boolean' ? true : false;
+// }
+
+// console.log(booWho(null)); /* should return false */
+// console.log(booWho(true)); /* should return true */
+// console.log(booWho(false)); /* should return true */
+// console.log(booWho([1, 2, 3])); /* should return false */
+// console.log(booWho([].slice)); /* should return false */
+// console.log(booWho({ "a": 1 })); /* should return false */
+// console.log(booWho(1)); /* should return false */
+// console.log(booWho(NaN)); /* should return false */
+// console.log(booWho("a")); /* should return false */
+// console.log(booWho("true")); /* should return false */
+// console.log(booWho("false")); /* should return false */
