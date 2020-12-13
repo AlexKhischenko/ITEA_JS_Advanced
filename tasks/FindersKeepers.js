8b0cbbac3d5ef68f377b66460e1a21f33376f3d9
Aleksandr Khischenko <khischenko.a@gmail.com>
2020-12-13 11:52:39 +0200

    added file FindersKeepers.js

---
+++ tasks/FindersKeepers.js
@@ -0,0 +1,31 @@
+/*
+Task
+Algorithm Scripting: Finders Keepers
+*/
+
+/* ---------- Imperative method ---------- */
+
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0)); /* should return 2 */
+console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); /* should return 8 */
+console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); /* should return undefined */
+
+/* ---------- Declarative method ---------- */
+
+// function findElement(arr, func) {
+//   return arr.find(function(item) {
+//     return func(item) === true ? item : undefined;
+//   });
+// }
+
+// console.log(findElement([1, 2, 3, 4], num => num % 2 === 0)); /* should return 2 */
+// console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); /* should return 8 */
+// console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); /* should return undefined */
