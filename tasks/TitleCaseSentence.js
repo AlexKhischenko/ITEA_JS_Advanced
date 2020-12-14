1ece36bbff2f0c79be1d88e0c71d68103d505e6d
Aleksandr Khischenko <khischenko.a@gmail.com>
2020-12-14 18:58:17 +0200

    added TitleCaseSentence.js - imperative method

---
+++ tasks/TitleCaseSentence.js
@@ -0,0 +1,31 @@
+
+/*
+Task
+Algorithm Scripting: Title Case a Sentence
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+*/
+
+/* ---------- Imperative method ---------- */
+
+function titleCase(str) {
+  var tempStr = str.toLowerCase(),
+      newStr = tempStr[0].toUpperCase(),
+      i = 1;
+      // debugger;
+  for (i; i < tempStr.length; i += 1) {
+    if (tempStr[i] === ' ') {
+      newStr = newStr + tempStr[i] + tempStr[i + 1].toUpperCase();
+      i += 1;
+    } else {
+      newStr += tempStr[i];
+    }
+  }
+  return newStr;
+}
+
+console.log(titleCase("I'm a little tea pot")); /* should return I'm A Little Tea Pot */
+console.log(titleCase("sHoRt AnD sToUt")); /* should return Short And Stout */
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")); /* should return Here Is My Handle Here Is My Spout */
+
+/* ---------- Declarative method ---------- */
