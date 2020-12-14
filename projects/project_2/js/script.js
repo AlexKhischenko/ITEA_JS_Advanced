fa0022ffef5701d7052fa2639a2a627b8355a039
Aleksandr Khischenko <khischenko.a@gmail.com>
2020-12-14 13:53:49 +0200

    code refactoring

--- projects/project_2/js/script.js
+++ projects/project_2/js/script.js
@@ -20,30 +20,35 @@ var weatherCurrentLocation = document.querySelector('.weather-current-city'),
     weatherBtn = document.querySelector('.btn-one'),
     cityNameBtn = document.querySelector('.btn-two'),
     cityName = document.querySelector('.weather-city-name'),
+    weatherDescription = document.querySelector('.weather-description'),
+    weatherIcon = document.querySelector('.weather-icon'),
+    loader = document.querySelector('.cs-loader'),
     feels = 'feels_like',
     cityNameValue;
 
 /* Getting coordinates */
 function getCurrentLocationCoordinates() {
   if (navigator.geolocation) { /* check if user brouser supoport navigation */
+    getUserLocation();
     navigator.geolocation.getCurrentPosition(function(position) {
-      var currentLatitude = position.coords.latitude,
-          currentLongitude = position.coords.longitude;
-      getWeatherData(currentLatitude, currentLongitude);
+      getWeatherData(position.coords.latitude, position.coords.longitude);
     });
   } else { /* ask to use another browser */
     alert('Could you try to use another browser because current one does not support a navigation!');
   }
 }
 
-/* Getting city name */
+/* Getting local city name */
 function getCityName() {
   cityNameValue = cityName.value;
+  weatherCurrentLocation.innerText = cityName.value;
+  cityName.value = ''; /* Clear placeholder */
   getWeatherData();
 }
 
 /* Getting json response with weather data */
 function getWeatherData(latitude, longitude) {
+  loader.classList.remove('hidden');
   var api;
   if (latitude && longitude) {
     api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=66d7c3fb840d30a551d86ce85c5f9832&units=metric`;
@@ -55,7 +60,9 @@ function getWeatherData(latitude, longitude) {
       return response.json();
     })
     .then(function(recievedData) {
+      console.log(recievedData);
       displayRecievedWeatherData(recievedData);
+      loader.classList.add('hidden');
     });
 }
 
@@ -70,22 +77,38 @@ function getUserLocation() {
     });
 }
 
+/* Display local user location */
+function displayUserLocation(recievedLocationData) {
+  weatherCurrentLocation.innerText = `${recievedLocationData.city}`;
+}
+
 function displayRecievedWeatherData(recievedWeatherData) {
-  currentTemperature.innerText = recievedWeatherData.main.temp;
-  feelsLike.innerText = recievedWeatherData.main[feels];
+  switch(recievedWeatherData.weather[0].main) {
+    case 'Rain':
+      weatherIcon.innerHTML = '<i class="fas fa-cloud-rain"></i>';
+      break;
+    case 'Snow':
+      weatherIcon.innerHTML = '<i class="far fa-snowflake"></i>';
+      break;
+    case 'Clouds':
+      weatherIcon.innerHTML = '<i class="fas fa-cloud"></i>';
+      break;
+    case 'Clear':
+      weatherIcon.innerHTML = '<i class="fas fa-sun"></i>';
+      break;
+    default:
+      weatherIcon.innerHTML = '<i class="fas fa-cloud-sun"></i>';
+      break;
+  }
+  currentTemperature.innerText = Math.ceil(recievedWeatherData.main.temp);
+  feelsLike.innerText = Math.ceil(recievedWeatherData.main[feels]);
   pressure.innerText = recievedWeatherData.main.pressure;
   humidity.innerText = recievedWeatherData.main.humidity;
   windSpeed.innerText = recievedWeatherData.wind.speed;
   clouds.innerText = recievedWeatherData.clouds.all;
   visibility.innerText = recievedWeatherData.visibility;
+  weatherDescription.innerText = recievedWeatherData.weather[0].description;
 }
-
-function displayUserLocation(recievedLocationData) {
-  weatherCurrentLocation.innerText = `${recievedLocationData.city}, ${recievedLocationData.country}`;
-}
-
-
-getUserLocation();
 
 /* Add event listener for the first part (current location) */
 weatherBtn.addEventListener('click', getCurrentLocationCoordinates);
