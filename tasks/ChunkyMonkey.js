be25ae42dda9aec1b66c7e380bc40df1796a767d
Alex Khischenko <Khischenko.A@gmail.com>
2020-12-18 13:45:07 +0200

    ChunkyMonkey.js - added imperative and declarative methods

--- tasks/ChunkyMonkey.js
+++ tasks/ChunkyMonkey.js
@@ -7,7 +7,25 @@ Write a function that splits an array (first argument) into groups the length of
 /* ---------- Imperative method ---------- */
 
 function chunkArrayInGroups(arr, size) {
-  return arr;
+  var arrSize = arr.length,
+      newArr = [],
+      innerArr = [],
+      counter = 0,
+      i = 0;
+  for (i; i < arrSize; i += 1) {
+    innerArr.push(arr[i]);
+    ++counter;
+    if (counter === size) {
+      counter = 0;
+      newArr.push(innerArr);
+      innerArr = [];
+      continue;
+    }
+    if (i === arrSize - 1) {
+      newArr.push(innerArr);
+    }
+  }
+  return newArr;
 }
 
 console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2)); /* should return [["a", "b"], ["c", "d"]] */
@@ -19,3 +37,12 @@ console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)); /* should retur
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)); /* should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]] */
 
 /* ---------- Declarative method ---------- */
+
+// function chunkArrayInGroups(arr, size) {
+//   var anewArr = [],
+//       arrSize = arr.length;
+//   for (var i = 0; i < arrSize; i += size) {
+//     anewArr.push(arr.slice(i, i + size));
+//   }
+//   return anewArr;
+// }
