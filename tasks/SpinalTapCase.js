68607ce654cd9c548247f636fbc50cf926b0b91d
Aleksandr Khischenko <khischenko.a@gmail.com>
2021-01-13 23:41:46 +0200

    added solution for: SpinalTapCase.js

--- tasks/SpinalTapCase.js
+++ tasks/SpinalTapCase.js
@@ -10,21 +10,38 @@ For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { f
 function spinalCase(str) {
   var size = str.length,
       i = 1,
-      newStr = str[0].toLowerCase();
-// debugger;
+      j = 0,
+      toggle = 0,
+      tempStr = str[0].toLowerCase(),
+      newStr = '';
+
   for (i; i < size; i += 1) {
-    if (str[i] === str[i].toUpperCase() && str[i] !== ' ' && str[i] !== '_') {
-      newStr += '-' + str[i].toLowerCase();
+    if (str[i] === str[i].toUpperCase() && str[i] !== ' ' && str[i] !== '_' && str[i] !== '-') {
+      tempStr += '-' + str[i].toLowerCase();
     } else {
       switch(str[i]) {
-        case ' ':
         case '_':
-          newStr += '-';
+          tempStr += '';
           break;
         default:
-          newStr += str[i];
-          break
+          tempStr += str[i];
+          break;
       }
+    }
+  }
+
+  size = tempStr.length;
+  for (j; j < size; j += 1) {
+    if (tempStr[j] === ' ') {
+      toggle = 1;
+    } else if (tempStr[j] === '-' && toggle === 1) {
+      newStr += tempStr[j];
+      toggle = 0;
+    } else if (tempStr[j] !== '-' && toggle === 1) {
+      newStr += '-' + tempStr[j];
+      toggle = 0;
+    } else {
+      newStr += tempStr[j];
     }
   }
 
@@ -40,3 +57,9 @@ console.log(spinalCase("AllThe-small Things")); /* should return "all-the-small-
 
 
 /* ---------- Declarative method ---------- */
+
+// function spinalCase(str) {
+//   var regularExpression = /\s|_|(?=[A-Z])/;
+  
+//   return str.split(regularExpression).join('-').toLowerCase();
+// }
