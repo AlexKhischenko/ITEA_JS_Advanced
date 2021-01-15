cd82f808425b778f018f9ea5bf834d9016a55a99
Alex Khischenko <Khischenko.A@gmail.com>
2021-01-15 17:53:19 +0200

    project_6: hide a list, Capital letters, population

--- projects/project_6_searchCityOrState/js/script.js
+++ projects/project_6_searchCityOrState/js/script.js
@@ -5,30 +5,49 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 
+showDefaultStructure();
+
 fetch(endpoint)
-    .then(function (blob) {
-        return blob.json();
-    })
-    .then(function (data) {
-        cities.push(...data);
-    });
+  .then(function (blob) {
+      return blob.json();
+  })
+  .then(function (data) {
+      cities.push(...data);
+  });
 
 function findMatches(text, cities) {
-    return cities.filter(function (place) {
-        return place.city.includes(text) || place.state.includes(text);
-    })
+  if (text === '' || text === ' ') {
+    return false;
+  }
+  return cities.filter(function (place) {
+    if (place.city.toLowerCase().includes(text) || place.state.toLowerCase().includes(text))
+    return place;
+  })
+}
+
+function showDefaultStructure() {
+  suggestions.innerHTML = `
+    <li>Filter for a city</li>
+    <li>Or a state</li>
+  `
 }
 
 function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    suggestions.innerHTML = matchArray.map(function (place) {
-        return `
-            <li>
-                <span>${place.city} ${place.state}</span>
-            </li>
-        `
-    }).join('');
+  const matchArray = findMatches(this.value.toLowerCase(), cities);
+  let inputValue = this.value;
+  let inputValueWithMark = `<mark>${inputValue}</mark>`;
 
+  if (matchArray) {
+    suggestions.innerHTML = matchArray.map(function (place) {
+      return `
+        <li>
+        <div><span class="description">State:</span> ${place.state.replace(inputValue, inputValueWithMark)}</div>
+        <div><span class="description">City:</span> ${place.city.replace(inputValue, inputValueWithMark)}</div>
+        <div><span class="description">Population:</span> ${place.population}</div>
+        </li>
+      `
+    }).join('');
+  } else showDefaultStructure();
 }
 
 searchInput.addEventListener('keyup', displayMatches);
