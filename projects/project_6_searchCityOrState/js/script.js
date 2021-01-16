c13ea218213f12038fe699dd28104144abecae23
Aleksandr Khischenko <khischenko.a@gmail.com>
2021-01-16 18:12:11 +0200

    project_6: added whole solution and tested

--- projects/project_6_searchCityOrState/js/script.js
+++ projects/project_6_searchCityOrState/js/script.js
@@ -1,5 +1,7 @@
+// Variables declaration
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
+const closeBtn = document.querySelector('.close');
 
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
@@ -7,6 +9,7 @@ const cities = [];
 
 showDefaultStructure();
 
+// Sending a request and getting a response
 fetch(endpoint)
   .then(function (blob) {
       return blob.json();
@@ -15,39 +18,6 @@ fetch(endpoint)
       cities.push(...data);
   });
 
-function findMatches(text, cities) {
-  if (text === '' || text === ' ') {
-    return false;
-  }
-  return cities.filter(function (place) {
-    if (place.city.toLowerCase().includes(text) || place.state.toLowerCase().includes(text))
-    return place;
-  })
-}
-
-function showDefaultStructure() {
-  suggestions.innerHTML = `
-    <li>Filter for a city</li>
-    <li>Or a state</li>
-  `
-}
-
-function displayMatches() {
-  const matchArray = findMatches(this.value.toLowerCase(), cities);
-  let inputValue = this.value;
-  let inputValueWithMark = `<mark>${inputValue}</mark>`;
-
-  if (matchArray) {
-    suggestions.innerHTML = matchArray.map(function (place) {
-      return `
-        <li>
-        <div><span class="description">State:</span> ${place.state.replace(inputValue, inputValueWithMark)}</div>
-        <div><span class="description">City:</span> ${place.city.replace(inputValue, inputValueWithMark)}</div>
-        <div><span class="description">Population:</span> ${place.population}</div>
-        </li>
-      `
-    }).join('');
-  } else showDefaultStructure();
-}
-
+// Events listeners
 searchInput.addEventListener('keyup', displayMatches);
+closeBtn.addEventListener('click', clearSearchInput);
